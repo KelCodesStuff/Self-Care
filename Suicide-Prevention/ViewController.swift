cbe17aa02434871b5b05e67de71d99404e5173dc
Kelvin Reid <codesmithk@gmail.com>
2018-09-12 23:50:07 -0400

    Adding main files

---
+++ Suicide-Prevention/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Suicide-Prevention
+//
+//  Created by Kelvin Reid on 9/12/18.
+//  Copyright © 2018 KelReid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
