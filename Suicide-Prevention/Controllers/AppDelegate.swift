f7fd937ebda9e98cff914f715e06b30332493334
KelCodesStuff <codesmithk@gmail.com>
2018-10-25 00:34:54 -0400

    Updating copyright

--- Suicide-Prevention/Controllers/AppDelegate.swift
+++ Suicide-Prevention/Controllers/AppDelegate.swift
@@ -1,22 +1,12 @@
 /// Copyright (c) 2018 
 
 import UIKit
-import Sentry
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     AppController.shared.show(in: UIWindow(frame: UIScreen.main.bounds))
-    
-    // Create a Sentry client and start crash handler
-    do {
-        Client.shared = try Client(dsn: "https://3122d527176d44aa8beccdc1eb066ad5@sentry.io/1304899")
-        try Client.shared?.startCrashHandler()
-    } catch let error {
-        print("\(error)")
-    }
-    
     return true
   }
 }
