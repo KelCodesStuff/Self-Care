67f09c955455360943842eabf3a4a1ec25d9cd18
Kelvin Reid <codesmithk@gmail.com>
2019-01-27 19:52:48 -0500

    Importing firebase module into testing files

--- SelfCareTests/SelfCareTests.swift
+++ SelfCareTests/SelfCareTests.swift
@@ -7,6 +7,8 @@
 //
 
 import XCTest
+import Firebase
+
 @testable import SelfCare
 
 class SelfCareTests: XCTestCase {
--- SelfCareUITests/SelfCareUITests.swift
+++ SelfCareUITests/SelfCareUITests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+import Firebase
 
 class SelfCareUITests: XCTestCase {
 
