35bc480a1a2079e823a4556c83311028c16426e6
Kelvin Reid <codesmithk@gmail.com>
2019-01-27 19:52:05 -0500

    Adding wine controller file

---
+++ SelfCare/Controller/WineController.swift
@@ -0,0 +1,18 @@
+//
+//  WineController.swift
+//  
+//
+//  Created by Kelvin Reid on 1/27/19.
+//
+
+import UIKit
+import Firebase
+
+class WineController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
