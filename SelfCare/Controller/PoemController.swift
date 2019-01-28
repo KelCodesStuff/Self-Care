8c8789bc665505d20c74265e1cff324de81bd9a0
Kelvin Reid <codesmithk@gmail.com>
2019-01-27 19:51:52 -0500

    Adding poem controller file

---
+++ SelfCare/Controller/PoemController.swift
@@ -0,0 +1,19 @@
+//
+//  PoemController.swift
+//  SelfCare
+//
+//  Created by Kelvin Reid on 1/27/19.
+//  Copyright © 2019 KReid. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class PoemController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
