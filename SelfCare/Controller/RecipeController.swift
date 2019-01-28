0bebbbe8938329af8f7538b184a544a55dd0699c
Kelvin Reid <codesmithk@gmail.com>
2019-01-27 19:52:16 -0500

    Adding recipe controller file

---
+++ SelfCare/Controller/RecipeController.swift
@@ -0,0 +1,19 @@
+//
+//  RecipeController.swift
+//  SelfCare
+//
+//  Created by Kelvin Reid on 1/27/19.
+//  Copyright © 2019 KReid. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class RecipeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
