6d0e3149711f760866c92c3b1c438a278baa98f9
Kelvin Reid <codesmithk@gmail.com>
2019-06-22 18:05:57 -0400

    Create UIView+Additions.swift

---
+++ RWRC/Extensions/UIView+Additions.swift
@@ -0,0 +1,23 @@
+//
+//  MenuViewController.swift
+//  SelfCare
+//
+//  Created by Kelvin Reid on 10/01/18.
+//  Copyright © 2019 KReid. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+  
+  func smoothRoundCorners(to radius: CGFloat) {
+    let maskLayer = CAShapeLayer()
+    maskLayer.path = UIBezierPath(
+      roundedRect: bounds,
+      cornerRadius: radius
+    ).cgPath
+    
+    layer.mask = maskLayer
+  }
+  
+}
